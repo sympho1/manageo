ed5e3d7f78fd00b4becd5e77faafcb316819e466
Brice Gnoboa <symphognoboa@gmail.com>
2021-06-29 14:09:46 +0000

    first commit - change the app title

--- manageo/main.py
+++ manageo/main.py
@@ -8,7 +8,7 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 class Manageo(App):
     sm = ScreenManager()
     def build(self):
-        self.title = "Lambert"
+        self.title = "Manageo"
         self.sm.add_widget(LoginScreen(name="login"))
         self.sm.add_widget(RegisterScreen(name="register"))
         self.sm.add_widget(HomeScreen(name="home_screen"))
